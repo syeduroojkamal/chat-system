0321c85708ef5db5041b6d99314db995b03f6786
syeduroojkamal <syeduroojkamal26@gmail.com>
2025-05-14 23:08:57 +0530

    feat: add sign-in and sign-up pages using Clerk components
    feat: implement UserSelector component for user selection in chats
    
    feat: create reusable Avatar component for user avatars
    
    feat: develop Card component for consistent UI layout
    
    feat: introduce Input component for form handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,7 @@
-import { type Metadata } from "next";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { type Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,15 +29,6 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           {children}
         </body>
       </html>
---
+++ components/UserSelector.tsx
@@ -0,0 +1,28 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { User } from "lucide-react";
+import { Card } from "./ui/card";
+import Link from "next/link";
+
+type UserSelectorProps = {
+  id: number;
+  name: string;
+};
+
+export default function UserSelector({ id, name }: UserSelectorProps) {
+  return (
+    <Link href={`/chats/${id}`}>
+      <Card className="flex-row items-center">
+        <Avatar className="h-12 w-12 ml-4">
+          <AvatarImage src="" alt="@shadcn" />
+          <AvatarFallback>
+            <User />
+          </AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col space-y-1">
+          <p className="text-xl leading-none">{name}</p>
+          <p className="text-sm text-muted-foreground">Last Message</p>
+        </div>
+      </Card>
+    </Link>
+  );
+}
