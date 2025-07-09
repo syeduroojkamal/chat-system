17e46540fbcc85b2eacae75c6f62d510af37dfb6
syeduroojkamal <syeduroojkamal26@gmail.com>
2025-07-09 19:49:15 +0530

    feat: read indicator conditional redendering

--- src/app/user/[userId]/MessageBubble.tsx
+++ src/app/user/[userId]/MessageBubble.tsx
@@ -1,6 +1,7 @@
 import { Message } from "@/lib/messageStore";
 import { useUserStore } from "@/lib/userStore";
 import { formatTimestamp } from "@/lib/utils";
+import { Check, CheckCheck } from "lucide-react";
 
 export default function MessageBubble({ message }: { message: Message }) {
   const userState = useUserStore((state) => state.user);
@@ -10,20 +11,25 @@ export default function MessageBubble({ message }: { message: Message }) {
     return (
       <div className="self-end bg-primary text-primary-foreground px-4 py-2 rounded-md max-w-2/3 flex flex-col items-end border shadow">
         <p>{message.message}</p>
-        <p className="text-xs text-primary-foreground/70">
-          {formatTimestamp(message.timestamp)}
-        </p>
+        <div className="flex justify-end items-center text-xs gap-1">
+          <p className="text-xs text-primary-foreground/70">
+            {formatTimestamp(message.timestamp)}
+          </p>
+          {message.read ? (
+            <CheckCheck className="size-4" />
+          ) : (
+            <Check className="size-4" />
+          )}
+        </div>
       </div>
     );
   }
   return (
     <div className="self-start bg-muted px-4 py-2 rounded-md max-w-2/3 flex flex-col items-start border shadow">
       <p>{message.message}</p>
-      {message.timestamp && (
-        <p className="text-xs text-muted-foreground/60">
-          {formatTimestamp(message.timestamp)}
-        </p>
-      )}
+      <p className="text-xs text-muted-foreground/60">
+        {formatTimestamp(message.timestamp)}
+      </p>
     </div>
   );
 }
